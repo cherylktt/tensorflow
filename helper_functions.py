3ee8f1f395277a303c8b9adc881c1de3d0462050
ck <65394936+cherylktt@users.noreply.github.com>
2022-04-26 21:39:52 +0800

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -308,6 +308,30 @@ def pred_custom_image(model, filepath):
         plt.title(f"prediction: {pred_class}\n probability: {pred_prob.max():.2f}")
         plt.axis(False);
 
+# Function to evalute model's predictions using accuracy, precision, recall, f1_score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+  
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+
+  # Calcaulte model precision, precision and f1-score using "weighted" average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                   "precision": model_precision,
+                   "recall": model_recall,
+                   "f1": model_f1}
+  return model_results
+        
 # Note: The following confusion matrix code is a remix of Scikit-Learn's 
 # plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
 import itertools
